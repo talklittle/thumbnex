e45acf390299c6acde440af538bfe90e16413626
Andrew Shu <talklittle@gmail.com>
2016-08-17 11:42:06 -0700

    mix: drop Elixir to 1.0 (was 1.3)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Thumbnex.Mixfile do
   def project do
     [app: :thumbnex,
      version: "0.1.0",
-     elixir: "~> 1.3",
+     elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
