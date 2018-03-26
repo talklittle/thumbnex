da1a253d9717f4a128ba501fde66eaa929e3fce7
Andrew Shu <talklittle@gmail.com>
2018-03-26 15:35:21 -0700

    deps: ffmpex 0.5.0 (was 0.4.0), elixir 1.4 (was 1.0)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Thumbnex.Mixfile do
   def project do
     [app: :thumbnex,
      version: "0.2.4",
-     elixir: "~> 1.0",
+     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
@@ -16,7 +16,7 @@ defmodule Thumbnex.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [extra_applications: [:logger]]
   end
 
   # Dependencies can be Hex packages:
@@ -30,7 +30,7 @@ defmodule Thumbnex.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [{:mogrify, "~> 0.5.4"},
-     {:ffmpex, "~> 0.4.1"},
+     {:ffmpex, "~> 0.5.0"},
      {:ex_doc, ">= 0.0.0", only: :dev}]
   end
 
